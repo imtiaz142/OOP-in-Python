89c2b2efa7cc7e06c6777334e27f7b4a2c503e7a
imtiaz142 <imtiaz8182@gmail.com>
2025-07-30 02:32:19 -0700

    init is added

--- Practice.py
+++ Practice.py
@@ -1,17 +1,25 @@
-class personal:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    
-    def greet(self):
-        print(f"Hello, my name is {self.name} and i am {self.age} years old")
-    
-personal1 = personal("ali",24)
-personal1.greet()
 
-personal1 = personal("ahmad",24)
-personal1.greet()
-personal1 = personal("nawaz",24)
-personal1.greet()
-personal1 = personal("sabtan",24)
-personal1.greet()
+class Mobile:
+    def __init__(self,brand,price):
+        self.brand=brand
+        self.price=price
+
+    def show_info(self):
+        print(f"the brand name is {self.brand},and price is {self.price}")
+    
+brand_info = Mobile("nokia",234)
+brand_info.show_info()
+
+
+
+class Mobile:
+    def mobile_info(self,brand,price):
+        self.brand=brand
+        self.price=price
+    def show_info(self):
+        print(f"the brand name is {self.brand},and price is {self.price}")
+m1= Mobile()
+m1.mobile_info("NOkia",45)
+m1.show_info()
+
+    
